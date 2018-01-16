a38e9d8559a7b6066ea11fd28c5f3b3a8f6820cc
michimau <mauro.michielon@eea.europa.eu>
2018-01-16 10:08:52 +0100

    major fixes

--- elasticsearch/docker-entrypoint.sh
+++ elasticsearch/docker-entrypoint.sh
@@ -3,37 +3,40 @@
 set -e
 
 # Add elasticsearch as command if needed
-if [ "${1:0:1}" = '-' ]; then
+if [[ "${1:0:1}" = '-' ]]; then
 	set -- elasticsearch "$@"
 fi
 
 cp /elasticsearch.yml /usr/share/elasticsearch/config/elasticsearch.yml
 
-if [ $ELASTICSEARCH_MASTER = "YES" ]
-then
-  
-  cat /readonlyrest.yml >> /usr/share/elasticsearch/config/elasticsearch.yml
+if [[ $ELASTICSEARCH_MASTER = "YES" ]]; then
+
+   cp /readonlyrest.yml /usr/share/elasticsearch/config/readonlyrest.yml 
+#  cat /readonlyrest.yml >> /usr/share/elasticsearch/config/elasticsearch.yml
 
   export TESTREADONLYREST=$(/usr/share/elasticsearch/bin/elasticsearch-plugin list | grep readonlyrest)
-  if [ -z "$TESTREADONLYREST" ]
-  then
+  if [[ -z "$TESTREADONLYREST" ]]; then
     bin/elasticsearch-plugin install file:/usr/share/elasticsearch/readonlyrest.zip
   fi
   export TESTREADONLYREST=''
 
   rm -rf /tmp/ssl
   mkdir -p /tmp/ssl
-  keytool -genkey -keyalg RSA -noprompt -alias $SERVERNAME -dname "CN=$SERVERNAME,OU=IDM,O=EEA,L=IDM1,C=DK" -keystore /tmp/ssl/self.jks -storepass $KIBANA_RW_PASSWORD -keypass $KIBANA_RW_PASSWORD
-  keytool -keystore  /tmp/ssl/self.jks -alias $SERVERNAME -export -file  /tmp/ssl/self.cert
-
+  keytool -genkey -keyalg RSA -noprompt -alias $SERVERNAME -dname "CN=$SERVERNAME,OU=IDM,O=EEA,L=IDM1,C=DK" -deststoretype pkcs12 -keystore /tmp/ssl/self.jks -storepass $KIBANA_RW_PASSWORD -keypass $KIBANA_RW_PASSWORD
+  keytool -keystore  /tmp/ssl/self.jks -alias $SERVERNAME -export -file  /tmp/ssl/self.cert -srcstorepass $KIBANA_RW_PASSWORD -deststorepass $KIBANA_RW_PASSWORD
+  
   chmod 400 /tmp/ssl/*
 
-  sed "s#CHECKHEALTH#$CHECKHEALTH#g" -i /usr/share/elasticsearch/config/elasticsearch.yml
-
+  #sed "s#CHECKHEALTH#$CHECKHEALTH#g" -i /usr/share/elasticsearch/config/elasticsearch.yml
+  sed "s#CHECKHEALTH#$CHECKHEALTH#g" -i /usr/share/elasticsearch/config/readonlyrest.yml
+  
   echo "node.master: true" >> /usr/share/elasticsearch/config/elasticsearch.yml
+  echo "http.type: ssl_netty4" >> /usr/share/elasticsearch/config/elasticsearch.yml
 
   sed "s#KIBANA_RW_USERNAME#$KIBANA_RW_USERNAME#g" -i /usr/share/elasticsearch/config/elasticsearch.yml
   sed "s#KIBANA_RW_PASSWORD#$KIBANA_RW_PASSWORD#g" -i /usr/share/elasticsearch/config/elasticsearch.yml
+  sed "s#KIBANA_RW_USERNAME#$KIBANA_RW_USERNAME#g" -i /usr/share/elasticsearch/config/readonlyrest.yml
+  sed "s#KIBANA_RW_PASSWORD#$KIBANA_RW_PASSWORD#g" -i /usr/share/elasticsearch/config/readonlyrest.yml
 
   sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /usr/share/elasticsearch/config/elasticsearch.yml
   sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /usr/share/elasticsearch/config/elasticsearch.yml
